abd900b4de4ac90340495e7d9e38889bbbfad095
Bw32Samba <cord1dile@gmail.com>
2025-10-29 09:22:28 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+// ===== config.js =====
+module.exports = {
+  SERVER: {
+    PORT: 5000,
+    WEB_NAME: "Virgo Web by Rajukingz",
+    AUDIO: "/assets/raju.mp3"
+  },
+  TELEGRAM: {
+    BOT_TOKEN: "8232694593:AAEpUe-V9e-L_9YwRs0ZXYRVMEizOzAknME", // ganti dengan token bot Telegram kamu
+    OWNER_ID: 8134760879,          // ganti dengan ID Telegram owner
+    GROUP_USERNAME: "+TFwA9kAitwJhZmVl", // invite link grup (tanpa https://t.me/)
+    CHANNEL_USERNAME: "rajustoreselaludihati" // username channel publik (tanpa @)
+  },
+  FILES: {
+    LOGIN: "login.json",
+    GMAIL: "gmail.json",
+    VERIFIED: "verified.json"
+  }
+};
